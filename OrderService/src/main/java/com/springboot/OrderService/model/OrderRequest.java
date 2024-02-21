38e3a96563f3babcc560ba4f68a5c0557dc56aaa
Manikanta <manikantayarramsetti445@gmail.com>
2024-02-22 01:04:19 +0530

    place order

--- OrderService/src/main/java/com/springboot/OrderService/model/OrderRequest.java
+++ OrderService/src/main/java/com/springboot/OrderService/model/OrderRequest.java
@@ -1,7 +1,16 @@
 package com.springboot.OrderService.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class OrderRequest {
-    private long orderId;
+    private long productId;
     private long totalAmount;
     private long quantity;
     private PaymentMode paymentMode;
--- OrderService/src/main/java/com/springboot/OrderService/service/OrderServiceImpl.java
+++ OrderService/src/main/java/com/springboot/OrderService/service/OrderServiceImpl.java
@@ -1,10 +1,13 @@
 package com.springboot.OrderService.service;
 
+import com.springboot.OrderService.entity.Order;
 import com.springboot.OrderService.model.OrderRequest;
 import com.springboot.OrderService.repository.OrderRepository;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.time.Instant;
 
 @Service
 @Log4j2
@@ -15,6 +18,15 @@ public class OrderServiceImpl implements OrderService {
     @Override
     public long placeOrder(OrderRequest orderRequest) {
         log.info("Placing order request: {}", orderRequest);
-        return 0;
+        Order order = Order.builder()
+                .amount(orderRequest.getTotalAmount())
+                .orderStatus("CREATED")
+                .productId(orderRequest.getProductId())
+                .orderDate(Instant.now())
+                .quantity(orderRequest.getQuantity())
+                .build();
+        order = orderRepository.save(order);
+        log.info("Order places successfully with Order Id: {}", order.getId());
+        return order.getId();
     }
 }
